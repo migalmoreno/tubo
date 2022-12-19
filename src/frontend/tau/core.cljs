0dd14a00d40ddb3426f2222e57f141c30a273238
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-12-20 00:45:34 +0100

    feat: Add frontend entry point

--- src/frontend/tau/core.cljs
+++ src/frontend/tau/core.cljs
@@ -1,4 +1,22 @@
-(ns tau.core)
+(ns tau.core
+  (:require
+   [day8.re-frame.http-fx]
+   [reagent.dom :as rdom]
+   [re-frame.core :as rf]
+   [tau.events]
+   [tau.routes :as routes]
+   [tau.subs]
+   [tau.views :as views]))
 
-(defn mount-app
-  [])
+(defn ^:dev/after-load mount-root
+  []
+  (rf/clear-subscription-cache!)
+  (rdom/render
+   [views/app]
+   (.querySelector js/document "#app")))
+
+(defn ^:export init
+  []
+  (routes/start-routes!)
+  (rf/dispatch-sync [:initialize-db])
+  (mount-root))
