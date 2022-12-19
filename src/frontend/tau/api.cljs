d85306e3fa2a25bcace91565bbc9bfabd8e3796c
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-12-20 00:46:13 +0100

    feat: Add request helper for re-frame-http-fx

---
+++ src/frontend/tau/api.cljs
@@ -0,0 +1,15 @@
+(ns tau.api
+  (:require
+   [ajax.core :as ajax]))
+
+(defn get-request
+  ([uri on-success on-failure]
+   (get-request uri on-success on-failure {}))
+  ([uri on-success on-failure params]
+   {:http-xhrio {:method :get
+                 :uri uri
+                 :params params
+                 :format (ajax/json-request-format)
+                 :response-format (ajax/json-response-format {:keywords? true})
+                 :on-success on-success
+                 :on-failure on-failure}}))
