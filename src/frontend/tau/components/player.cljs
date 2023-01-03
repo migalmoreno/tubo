152da2b81b4d814e4332e051ba21625264c07454
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-01-03 15:47:18 +0100

    fix(frontend): Fix global player styles

--- src/frontend/tau/components/player.cljs
+++ src/frontend/tau/components/player.cljs
@@ -12,10 +12,13 @@
       [:div.sticky.bottom-0.z-50.bg-neutral-900.p-5.absolute
        {:style {:borderColor service-color :borderTopWidth "2px" :borderStyle "solid"}}
        [:div.flex.items-center.justify-between
-        [:div.flex.flex-col
-         [:a.text-xs
-          {:href (rfe/href :tau.router/stream nil {:url url})} name]
-         [:a.text-xs.text-gray-300
-          {:href (rfe/href :tau.router/channel nil {:url uploader-url})} uploader-name]]
-        [:audio {:src stream :controls true}]
-        [:i.fa-solid.fa-close.cursor-pointer {:on-click #(rf/dispatch [::events/toggle-global-player])}]]])))
+        [:div.flex.flex-wrap.items-center
+         [:div.flex.flex-col
+          [:a.text-xs
+           {:href (rfe/href :tau.router/stream nil {:url url})} name]
+          [:a.text-xs.text-gray-300
+           {:href (rfe/href :tau.router/channel nil {:url uploader-url})} uploader-name]]
+         [:div.px-2.py-0.md:pt-4
+          [:audio {:src stream :controls true}]]]
+        [:div.px-2
+         [:i.fa-solid.fa-close.cursor-pointer {:on-click #(rf/dispatch [::events/toggle-global-player])}]]]])))
