5d769697b3c353ed95ecd5522990594019d6ef53
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-05-17 20:47:23 +0200

    fix: Fix project source URL

--- src/frontend/tubo/views.cljs
+++ src/frontend/tubo/views.cljs
@@ -83,7 +83,7 @@
     [:div.flex.flex-col.justify-center.items-center
      [:div.flex.items-center.justify-center
       [:div.items-center
-       [:a.font-bold {:href "https://git.mianmoreno.com/tubo.git"} "tubo"]]
+       [:a.font-bold {:href "https://git.mianmoreno.com/tubo"} "tubo"]]
       [:div
        [:p.px-2 (str "2022-" (.getFullYear (js/Date.)))]]]]]])
 
