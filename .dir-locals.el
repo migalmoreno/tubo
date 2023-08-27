2127fffa87cd8023e39c059e7e50dcdda034cd3b
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-08-27 22:09:50 +0200

    chore: Add directory-local fill-column

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,14 +1,15 @@
-((nil . ((cider-preferred-build-tool . clojure-cli)
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((fill-column . 80)
+         (cider-preferred-build-tool . clojure-cli)
          (cider-clojure-cli-aliases . "-M:frontend")
          (cider-default-cljs-repl . custom)
-         (cider-custom-cljs-repl-init-form
-          . "(do (require '[shadow.cljs.devtools.api :as shadow])
-(require '[shadow.cljs.devtools.server :as server])
-(server/start!)
-(shadow/watch :tubo)
-(shadow/nrepl-select :tubo))")
+         (cider-custom-cljs-repl-init-form . "(do (require '[shadow.cljs.devtools.api :as shadow])\n(require '[shadow.cljs.devtools.server :as server])\n(server/start!)\n(shadow/watch :tubo)\n(shadow/nrepl-select :tubo))")
          (cider-merge-sessions . :project)
-         (eval . (if (not (boundp 'cider-jack-in-nrepl-middlewares))
+         (eval . (if
+                     (not
+                      (boundp 'cider-jack-in-nrepl-middlewares))
                      (require 'cider)
                    (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
                    (add-to-list 'cider-jack-in-nrepl-middlewares "cider.nrepl/cider-middleware")
