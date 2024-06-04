68a246eb0a84467b4a290e5bf914a83cd7529fd7
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-06-04 14:15:02 +0200

    fix: fix settings handlers

--- src/frontend/tubo/settings/views.cljs
+++ src/frontend/tubo/settings/views.cljs
@@ -26,10 +26,10 @@
       [select-input "Theme" :theme theme #{:auto :light :dark}]
       [select-input "Default service" :default-service (:id default-service)
        (map #(-> % :info :name) services)
-       #(rf/dispatch [:settings/change (..  % -target -value)])]
+       #(rf/dispatch [:settings/change-service (..  % -target -value)])]
       [select-input "Default kiosk" :default-service
        (:default-kiosk default-service) (:available-kiosks default-service)
-       #(rf/dispatch [:settings/change (..  % -target -value)])]
+       #(rf/dispatch [:settings/change-kiosk (..  % -target -value)])]
       [boolean-input "Show description" :show-description show-description]
       [boolean-input "Show comments" :show-comments show-comments]
       [boolean-input "Show related videos" :show-related show-related]]]))
