50ca1ec0cff2ab978945d660c7e1ad4da59a431e
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-11-05 00:03:33 +0100

    chore: add Nix flake and clean up ignore files

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs =
+    { nixpkgs, systems, ... }:
+    let
+      eachSystem =
+        f: nixpkgs.lib.genAttrs (import systems) (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zprint
+            clj-kondo
+          ];
+        };
+      });
+    };
+}
