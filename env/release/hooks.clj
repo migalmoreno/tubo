cadc62e6046abfb88f29f668c685b9ebad2b6791
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-09-26 00:24:29 +0200

    chore: remove esbuild and amend shadow-cljs config

--- env/release/hooks.clj
+++ env/release/hooks.clj
@@ -15,36 +15,11 @@
       :nofollow-links   true}))
   build-state)
 
-(defn postcss-watch
+(defn postcss
   {:shadow.build/stage :configure}
-  [build-state]
-  (proc/process
-   {:extra-env {"NODE_ENV" "development"}}
-   "./node_modules/.bin/postcss" "resources/src/index.css"
-   "-o"                          "resources/public/index.css"
-   "--verbose"                   "-w")
-  build-state)
-
-(defn postcss-release
-  {:shadow.build/stage :configure}
-  [build-state]
-  (proc/shell {:extra-env {"NODE_ENV" "production"}}
-              "./node_modules/.bin/postcss"
-              "resources/src/index.css"    "-o"
-              "resources/public/index.css" "--verbose")
-  build-state)
-
-(defn esbuild-watch
-  {:shadow.build/stage :configure}
-  [build-state]
-  (fs/create-dirs "target/js/")
-  (when-not (fs/exists? "target/js/index.js")
-    (fs/create-file "target/js/index.js"))
-  (proc/process "node" "scripts/build-libs.mjs" "--watch")
-  build-state)
-
-(defn esbuild-release
-  {:shadow.build/stage :flush}
-  [build-state]
-  (proc/shell "node" "scripts/build-libs.mjs")
+  [build-state src dst]
+  (let [cmd ["./node_modules/.bin/postcss" src "-o" dst "--verbose"]]
+    (case (:shadow.build/mode build-state)
+      :dev     (apply proc/process (conj cmd "-w"))
+      :release (apply proc/shell {:extra-env {"NODE_ENV" "production"}} cmd)))
   build-state)
